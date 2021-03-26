0daff8ded34ff1656fa95f6033aa04901c017735
Agustin Martinez <abel.am91@gmail.com>
2021-03-26 18:55:52 -0300

    enlace servicio

---
+++ app.js
@@ -0,0 +1,13 @@
+const btnclick = document.getElementById('btn-icon');
+const navbar = document.getElementById('nav');
+let cont = 0;
+btnclick.onclick = function(){
+    if (cont == 0){
+        navbar.classList.add("show");
+        cont = 1;
+    }
+    else if (cont == 1){
+        navbar.classList.remove("show");
+        cont = 0
+    }
+};
