34d77cbffe22a7f299c87b8c185b28036de9d493
Agustin Martinez <abel.am91@gmail.com>
2021-04-02 13:09:36 -0300

    2/4/21 subo todo :P

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const btnclick = document.getElementById('btn-icon');
-const navbar = document.getElementById('nav');
+const btnclick = document.getElementById("btn-icon");
+const navbar = document.getElementById("nav");
 let cont = 0;
-btnclick.onclick = function(){
-    if (cont == 0){
-        navbar.classList.add("show");
-        cont = 1;
-    }
-    else if (cont == 1){
-        navbar.classList.remove("show");
-        cont = 0
-    }
+btnclick.onclick = function () {
+  if (cont == 0) {
+    navbar.classList.add("show");
+    cont = 1;
+  } else if (cont == 1) {
+    navbar.classList.remove("show");
+    cont = 0;
+  }
 };
